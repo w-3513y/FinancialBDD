44a356ba5a308b745675a4bfd4b401a34d019809
Wesley <wesleyferreirasb@gmail.com>
2022-04-26 08:20:06 -0300

    Add project files.

---
+++ FinancialBDD/LoanPayment.cs
@@ -0,0 +1,10 @@
+﻿namespace FinancialBDD;
+
+public static class LoanPayment
+{
+    public static double Payment(double loan, double months, double interesRate)
+    {
+        return double.MinValue;
+    }
+
+}
